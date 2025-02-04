fa28c57727e061e3af2bdce134702fd466adac49
krishhaldwal <akrishjee@gmail.com>
2025-02-05 02:49:08 +0530

    added user and video models

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -21,7 +21,7 @@ const userSchema = new Schema(
             trim : true
         },
 
-        fullname : {
+        fullName : {
             type : String,
             required : true,
             trim : true,
@@ -60,4 +60,41 @@ const userSchema = new Schema(
     }
 )
 
-export const User = mongoose.model("User", userSchema)
+userSchema.pre("save", async function (next){
+
+    if( !this.isModified("password"))return next();
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password , this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+            emal : this.email,
+            username : this.username,
+            fullName : this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema) 
